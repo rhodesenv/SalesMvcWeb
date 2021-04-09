6cbf84e3463cf6b4eb4e387963dba6da86c29f90
ronaldohideki <ronaldo.ho@gmail.com>
2021-04-09 00:29:58 -0300

    Other Entities

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Sales { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus sales, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Sales = sales;
+            Seller = seller;
+        }
+    }
+}
